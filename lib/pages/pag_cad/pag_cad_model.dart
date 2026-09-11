9c1dc346d7d9b08f2df1992d8fa855e1f83477d3
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2026-09-11 12:31:31 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/pag_cad/pag_cad_model.dart
+++ lib/pages/pag_cad/pag_cad_model.dart
@@ -1,7 +1,12 @@
+import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
 import '/index.dart';
 import 'pag_cad_widget.dart' show PagCadWidget;
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 class PagCadModel extends FlutterFlowModel<PagCadWidget> {
   ///  State fields for stateful widgets in this page.
